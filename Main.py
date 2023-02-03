a636e6b801aa843ae26ed432b3c141e75ddff2d4
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2023-02-03 16:54:34 -0600

    adding functions

--- Main.py
+++ Main.py
@@ -1,16 +1,26 @@
 # Winthrop_VA_Project - A fun Voice Assistant from my friend's, Winthrop, voice.
 
-import regex
+import re
 
 def Read_File(file):
     with open(file,"r") as dt:
         data = dt.readlines()
     return data
 
+def Search_File(file):
+    for lines in file:
+        print(lines)
+        
 def main():
-    testing = Read_File("Querys_List/Compliments_List.txt")
-    print(testing)
-
+    
+    Compliments_List = Read_File("Querys_List/Compliments_List.txt")
+    Greetings_List = Read_File("Querys_List/Greetings_List.txt")
+    Insults_List = Read_File("Querys_List/Insults_List.txt")
+    Questions_List = Read_File("Querys_List/Questions_List.txt")
+    
+    Search_File(Compliments_List)
+        
+    
 #* use this to get certain lines [index]
 
 
