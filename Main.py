34578cadf6fed98cf91e8b73dede2591ca3783e4
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2023-02-03 16:32:52 -0600

    starting with the file reading mechanics

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
 # Winthrop_VA_Project - A fun Voice Assistant from my friend's, Winthrop, voice.
+
+import regex
+
+def Read_File(file):
+    with open(file,"r") as dt:
+        data = dt.readlines()
+    return data
+
+def main():
+    testing = Read_File("Querys_List/Compliments_List.txt")
+    print(testing)
+
+#* use this to get certain lines [index]
+
+
+
+
+if "__main__" == __name__: 
+    main()
