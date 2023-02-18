bcfeba82c32d4ac2800210fcaac9281e7b74ffb6
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2023-02-18 14:57:23 -0600

    almost finished with query system

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 
 #* TODO: get rid of \n characters in the lists and continue with the query system
 
-import re
+import sys
 import os
 import json
 import pygame
@@ -14,19 +14,28 @@ def main():
     print("#        Winthrop Voice Assistant Project!       #")
     print("#              By Brayden Cotterman              #")
     print("#" * 50)
-    start = input('\n\nPress enter to start!')
+    start = input('\nType Q at anytime to quit!\nPress enter to start!')
     # Nice intro page Me!
     
     Compliments_List = Read_File("Querys_List/Compliments_List.json")
     Greetings_List = Read_File("Querys_List/Greetings_List.json")
     Insults_List = Read_File("Querys_List/Insults_List.json")
     Questions_List = Read_File("Querys_List/Questions_List.json")
+    Query_list = [Compliments_List,Greetings_List,Insults_List,Questions_List]
     
-    Query = input('Winthrop is listening... type your query!\n')
-    if Query == "voiceline 1":
-        music(Compliments_List[Query])
-        e  = input()
-        
+    while True:
+        Query = input('Winthrop is listening... type your query!\n')
+        for List in Query_list:
+            if Query in List:
+                os.system('cls')
+                music(List[Query])
+                break
+            
+            elif Query == "q":
+                sys.exit()
+            
+            
+            
 def Read_File(file):
     with open(file, "r") as read_file:
         data = json.load(read_file)
@@ -38,7 +47,6 @@ def music(mp3):
     pygame.mixer.music.load(mp3)
     pygame.mixer.music.play()
   
-
 
 
 
